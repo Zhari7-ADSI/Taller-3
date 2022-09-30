5b70976ccfbbc9cc8b740ee5d7deec04e15a4005
Zhari7 <meneseszharick29@gmail.com>
2022-09-29 20:20:46 -0500

    Ejer_5

--- main.js
+++ main.js
@@ -3,17 +3,26 @@ addEventListener("DOMContentLoaded", (e) => {
     formulario.addEventListener("submit", (e) => {
         e.preventDefault();
 
-        let people = 40, woman = 1, men = 7, sum = 0;
+        let cont = 1, alum = 2;
 
-        while (sum <= people) {
-            sum = woman + men;
-            woman += 1;
-            men += 1;
+        while (cont <= alum){
+            let not1 = parseFloat(prompt(`Ingrese la primera nota del alumno ${cont}`));
+            let not2 = parseFloat(prompt(`Ingrese la segunda nota del alumno ${cont}`));
+            let not3 = parseFloat(prompt(`Ingrese la tercera nota del alumno ${cont}`));
+            let not4 = parseFloat(prompt(`Ingrese la cuarta nota del alumno ${cont}`));
+            let not5 = parseFloat(prompt(`Ingrese la quinta nota del alumno ${cont}`));
 
-            alert(`La cantidad de mujeres es de: ${woman}`);
-            alert(`La cantidad de hombres es de: ${men}`);
-            alert(`La cantidad total de mujeres es de: ${woman}`);
-            alert(`El total de personas es de: ${sum}`);
-        }
+            let nota = not1 + not2 + not3 + not4 + not5;
+            let prom = nota / 5;
+
+            if (prom < 3){
+                alert(`El alumno no puede presentar el examen, su promedio fué: ${prom}`);
+            }
+            else{
+                alert(`El alumno puede presentar el examen, su promedio fué: ${prom}`);
+            }
+            cont ++;
+        } 
+        
     })
 })
